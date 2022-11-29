2453d413f21751f9a2c5bd81bba0ea867ad636d6
Alyona <mirka-sobaka@inbox.ru>
2022-11-29 11:31:32 +0400

    lp 2 и ужасная задачка

--- homework2.py
+++ homework2.py
@@ -39,3 +39,29 @@
 # 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import Random, randint
+
+num = int(input('Введите число N: '))
+numbers = []
+for i in range(num):
+    numbers.append(randint(-num, num + 1))
+
+print(numbers)
+
+f = open('file.txt', 'w')
+while True:
+    s = input('Укажите индекс умножаемого элемента - ')
+    if s == "":
+        break
+    f.write(s + "\n")
+f.close()
+
+result = 1
+f = open('file.txt', 'r')
+for line in f:
+    if line == "":
+        break
+    result *= numbers[int(line)]
+f.close()
+print(result)
